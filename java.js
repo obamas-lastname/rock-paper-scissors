2ac9068173b347a9975d4fe5d5294869280d9d99
diana <diana.elena.petrosel@gmail.com>
2023-10-08 05:23:27 -0400

    Needs testing

---
+++ java.js
@@ -0,0 +1,36 @@
+
+function getComputerChoice(){
+    let arr = ["ROCK", "PAPER", "SCISSORS"];
+    let randomNumber = Math.floor(Math.random()*arr.length);
+    let randomElem = arr[randomNumber];
+    return randomElem;
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toLowerCase == "rock" && computerSelection == "PAPER"){
+        console.log("You lose! Paper beats rock")
+    }
+    else if(playerSelection.toLowerCase == "scissors" && computerSelection == "PAPER"){
+        console.log("You win! Scissors beats paper")
+    }
+    else if(playerSelection.toLowerCase == "paper" && computerSelection=="ROCK"){
+        console.log("You win! Paper beats rock")
+    }
+    else if(playerSelection.toLowerCase == "scissors" && computerSelection=="ROCK"){
+        console.log("You lose! Rock beats scissors")
+    }
+    else if(playerSelection.toLowerCase == "paper" && computerSelection=="SCISSORS"){
+        console.log("You lose! Scissors beats paper")
+    }
+    else if(playerSelection.toLowerCase == "rock" && computerSelection=="SCISSORS"){
+        console.log("You win! Rock beats scissors")
+    }
+    else if(playerSelection.toLowerCase == computerSelection.toLowerCase){
+        console.log("It's a draw!")
+    }
+}
+
+let playerSelection = "rock"
+let computerSelection = getComputerChoice()
+
+playRound(playerSelection, computerSelection)
