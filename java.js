39650b83f577f804ba4da46ea428418f17c60ee4
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2023-10-08 17:52:01 +0300

    Update java.js
    Change toLowerCase() and add user input

--- java.js
+++ java.js
@@ -1,4 +1,5 @@
 
+
 function getComputerChoice(){
     let arr = ["ROCK", "PAPER", "SCISSORS"];
     let randomNumber = Math.floor(Math.random()*arr.length);
@@ -6,31 +7,37 @@ function getComputerChoice(){
     return randomElem;
 }
 
+let computerChoice = getComputerChoice()
+
+function getPlayerInput(){
+    let answer = prompt("Choose between rock, paper, scissors:")
+    return answer
+}
+
 function playRound(playerSelection, computerSelection){
-    if(playerSelection.toLowerCase == "rock" && computerSelection == "PAPER"){
-        console.log("You lose! Paper beats rock")
+    if(playerSelection.toLowerCase() === "rock" && computerSelection === "PAPER"){
+        console.log("You lose! Paper beats rock");
     }
-    else if(playerSelection.toLowerCase == "scissors" && computerSelection == "PAPER"){
-        console.log("You win! Scissors beats paper")
+    else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "PAPER"){
+        console.log("You win! Scissors beats paper");
     }
-    else if(playerSelection.toLowerCase == "paper" && computerSelection=="ROCK"){
-        console.log("You win! Paper beats rock")
+    else if(playerSelection.toLowerCase() === "paper" && computerSelection==="ROCK"){
+        console.log("You win! Paper beats rock");
     }
-    else if(playerSelection.toLowerCase == "scissors" && computerSelection=="ROCK"){
-        console.log("You lose! Rock beats scissors")
+    else if(playerSelection.toLowerCase() == "scissors" && computerSelection==="ROCK"){
+        console.log("You lose! Rock beats scissors");
     }
-    else if(playerSelection.toLowerCase == "paper" && computerSelection=="SCISSORS"){
-        console.log("You lose! Scissors beats paper")
+    else if(playerSelection.toLowerCase() === "paper" && computerSelection==="SCISSORS"){
+        console.log("You lose! Scissors beats paper");
     }
-    else if(playerSelection.toLowerCase == "rock" && computerSelection=="SCISSORS"){
-        console.log("You win! Rock beats scissors")
+    else if(playerSelection.toLowerCase() === "rock" && computerSelection==="SCISSORS"){
+        console.log("You win! Rock beats scissors");
     }
-    else if(playerSelection.toLowerCase == computerSelection.toLowerCase){
-        console.log("It's a draw!")
+    else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        console.log("It's a draw!");
     }
 }
 
-let playerSelection = "rock"
-let computerSelection = getComputerChoice()
+let playerSelection = getPlayerInput()
 
-playRound(playerSelection, computerSelection)
+playRound(playerSelection, computerChoice)
