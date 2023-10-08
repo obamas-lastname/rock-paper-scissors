ef1a99b839748e25dae22e8552841d50071c06f1
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2023-10-08 18:04:53 +0300

    Add game function to java.js

--- java.js
+++ java.js
@@ -1,5 +1,4 @@
 
-
 function getComputerChoice(){
     let arr = ["ROCK", "PAPER", "SCISSORS"];
     let randomNumber = Math.floor(Math.random()*arr.length);
@@ -7,7 +6,7 @@ function getComputerChoice(){
     return randomElem;
 }
 
-let computerChoice = getComputerChoice()
+
 
 function getPlayerInput(){
     let answer = prompt("Choose between rock, paper, scissors:")
@@ -15,29 +14,55 @@ function getPlayerInput(){
 }
 
 function playRound(playerSelection, computerSelection){
+    let win=0;
     if(playerSelection.toLowerCase() === "rock" && computerSelection === "PAPER"){
         console.log("You lose! Paper beats rock");
+        win=1;
     }
     else if(playerSelection.toLowerCase() === "scissors" && computerSelection === "PAPER"){
         console.log("You win! Scissors beats paper");
+        win=2;
     }
     else if(playerSelection.toLowerCase() === "paper" && computerSelection==="ROCK"){
         console.log("You win! Paper beats rock");
+        win=2;
     }
     else if(playerSelection.toLowerCase() == "scissors" && computerSelection==="ROCK"){
         console.log("You lose! Rock beats scissors");
+        win=1;
     }
     else if(playerSelection.toLowerCase() === "paper" && computerSelection==="SCISSORS"){
         console.log("You lose! Scissors beats paper");
+        win=1;
     }
     else if(playerSelection.toLowerCase() === "rock" && computerSelection==="SCISSORS"){
         console.log("You win! Rock beats scissors");
+        win=2;
     }
     else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
         console.log("It's a draw!");
+        win=0;
     }
+    return win;
 }
 
-let playerSelection = getPlayerInput()
 
-playRound(playerSelection, computerChoice)
+
+
+function game(){
+    let i=1, playerScore=0, computerScore=0;
+    
+    while(i<=5){
+        let a = getPlayerInput();
+        let b = getComputerChoice();
+        let c= playRound(a, b);
+        if(c===2) playerScore++;
+        else if (c===1) computerScore++;
+        i++;
+    }
+
+     if(playerScore>computerScore) console.log("You win!");
+     else console.log("You lose!");
+}
+
+game();
